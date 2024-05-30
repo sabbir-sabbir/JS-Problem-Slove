b7ff42be357f405e8770b7933a5a3561492e022d
sabbir-sabbir <iosabbir7890@gmail.com>
2024-05-30 09:45:19 -0700

    first commit

---
+++ name.js
@@ -0,0 +1,134 @@
+// 1```````````` Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo" ; 
+
+
+// 2``````````On one single line, declare three variables with the following names and values:
+
+// So What Will Be The 1st Variable Name, Then 2nd Variable Values & 
+// 3rd Variable Name And Values Both! 
+
+let firstName = "John";  // write variable name
+let lastNamet = "Doe" //write variable values 
+let age = 35 ; // write variable name and values both 
+
+
+
+
+// 3``````````````` Use the correct assignment operator that will 
+// result in x being 50 (same as x = x * y). 
+
+x = 10;
+y = 5;
+x *= y // what will be the value of x ?  
+
+
+
+
+// 4```````````````Use comments to describe the correct data type of the following variables:
+
+let length = 16; // number 
+let lastName = "Johnson"; // string 
+
+const objj = {
+  firstName: "John",  
+  lastName: "Doe"
+};    // object
+
+
+
+
+
+// 5`````````````Execute the function named myFunction...
+ 
+ function myFunction() {
+  alert("Hello World!");
+}
+myFunction()
+
+
+
+
+
+// 6`````````````` Create an object called person with name = John, age = 50, Then,
+//    access the object to alert("John is 50")....
+
+   let person = {
+    name: 'john',
+    age: 50
+   };
+   alert(person.name + " is " + person.age);
+
+
+
+
+//7````````````````````` The <button> element should do something when someone clicks on it. Try to fix it!
+                   <button onclick="alert('Button clicked!')">Click me.</button> 
+
+
+
+
+
+// 8``````````````` Alert the number of items in an array, using the correct Array property: ..
+
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert('the number of items in an array: ' + cars.length);
+
+
+// 2. Change the first item of Brand to "Ford".
+
+	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+    Brand[0] = "Ford";
+
+
+
+ 
+    
+//    `````````````````````````9```````````````````````````````````````````````
+
+// 1. Use the correct Math method to create a random number.
+let x = Math.random(1,10);
+console.log(x);
+
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+let  y = Math.max(10, 20);
+console.log(y);
+
+
+// 3. Use the correct Math method to get the square root of 9.
+let sqr = Math.sqrt(9);
+console.log(sqr);
+
+
+
+
+
+// `````````````````````10`````````````````````````````
+// 1. Choose the correct comparison operator to alert true, when x is greater than y. 
+x = 10;
+y = 5;
+alert(x > y);      /**Completed */
+
+
+
+
+// 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+// let  age = ...;  /**Your value here */
+
+alert(age < 18 ? "Too young" : "Old enough");    /**Completed */
+
+
+
+
+/**hope to get the full numbers............... */
+
+
+
+
+
+
+
+
+
+
